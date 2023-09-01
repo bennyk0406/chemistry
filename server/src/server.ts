c469f86403803f778bde52bc75680c537f490d59
bennyk0406 <kbenny0406@gmail.com>
2023-09-01 23:57:46 +0900

    https

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,7 +10,7 @@ app.use(express.json())
 app.get("*", (_, res) => {
     res.sendFile(resolve(__dirname, "../../client/build/index.html"));
 })
-const server = app.listen(80, () => {
+const server = app.listen(223, () => {
     console.log("The server has started!")
 })
 
